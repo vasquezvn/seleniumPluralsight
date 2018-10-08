77aece99fc3bf42b57dc8206ce27624218a0ae0f
ivan <vasquez.vn@gmail.com>
2018-10-08 00:14:18 -0400

    updating code

--- WebDriverDemo/Program.cs
+++ WebDriverDemo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace WebDriverDemo
 {
@@ -6,9 +8,20 @@ namespace WebDriverDemo
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.google.es";
+
+            var searchBox = driver.FindElement(By.Id("lst-id"));
+            searchBox.SendKeys("pluralsight");
+            searchBox.Submit();
+
+            var imagesLink = driver.FindElement(By.ClassName("qs"));
+            imagesLink.Click();
+
+            var divContainer = driver.FindElement(By.Id("rg"));
+            var imageLink = divContainer.FindElements(By.ClassName("rg_1"))[0];
+            imageLink.Click();
+
         }
     }
 }
